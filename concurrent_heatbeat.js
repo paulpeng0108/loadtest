4081c584cdc8985cd07a8f8eb7f71f1487bcd92b
Paul Peng <99743244+paulpeng0108@users.noreply.github.com>
2022-08-26 10:56:30 -0400

    commit

--- concurrent_heatbeat.js
+++ concurrent_heatbeat.js
@@ -1,4 +1,15 @@
 import http from 'k6/http';
+import { SharedArray } from 'k6/data';
+import { sleep } from 'k6';
+
+const userPool = new SharedArray('userPool', () => JSON.parse(open('./userPool.json')))
+const playbackToken = userPool[Math.floor(Math.random() * userPool.length)]
+
+// export const options = {
+//     stages: [
+//         { duration: '10s', target: 10 }
+//     ]
+// };
 
 export const options = {
     stages: [
@@ -15,53 +26,7 @@ export const options = {
     ]
 };
 
-export function setup() {
-    let data = {
-        "AuthParameters" : {
-           "USERNAME" : "rokupaytest_1@nesn.com",
-           "PASSWORD" : "asdfasdf"
-        },
-        "AuthFlow" : "USER_PASSWORD_AUTH",
-        "ClientId" : "6ubem6njmkm3c0s69k1mvis288"
-     };
-
-    // Using a JSON string as body
-    let loginRes = http.post("https://cognito-idp.us-east-2.amazonaws.com/", JSON.stringify(data), {
-        headers: { 
-            'Content-Type': 'application/json',
-            "Content-Type": "application/x-amz-json-1.1",
-            "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth"
-        },
-    });
-
-
-    const userRes = http.get(
-        `https://b63dvs0d09.execute-api.us-east-2.amazonaws.com/loadtest/users/3dead207-ea7e-48fe-914d-5602aa233f18`,
-        {
-            headers:  {
-                'nesn-access-token': loginRes.json().AuthenticationResult.AccessToken,
-                'nesn-user-zipcode': "03218",
-                'nesn-device-id': "test-device-id-104",
-                "nesn-request-attempt":"nesn-request-attempt",
-                "nesn-location-check-type":"nesn-location-check-type",
-                "nesn-location-check-prompt":"nesn-location-check-prompt",
-                "nesn-state":"state",
-                "nesn-country":"country",
-                "nesn-video-id":"video",
-                "nesn-device-type":"device-type",
-                "nesn-device-hardware":"hardware",
-                "nesn-via-proxy":"true",
-                "nesn-mvpd":"mvpd",
-                "nesn-app-version": "version",
-                "nesn-video-asset-id": "-asset-id"
-            }
-        }
-    )
-
-    return userRes.json().subscription.playbackToken
-}
-
-export default function (playbackToken) {
+export default function () {
     const res = http.post(
         "https://b63dvs0d09.execute-api.us-east-2.amazonaws.com/loadtest/concurrent/heartbeat",
         null,
@@ -69,4 +34,6 @@ export default function (playbackToken) {
             headers:  {'nesn-playback-token': playbackToken}
         }
     )
+
+    //sleep()
 }

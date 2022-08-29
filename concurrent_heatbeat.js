73f3304c51a7d584aa73ee413d80dfa9eb9ca8b8
Paul Peng <99743244+paulpeng0108@users.noreply.github.com>
2022-08-29 09:05:58 -0400

    commit

--- concurrent_heatbeat.js
+++ concurrent_heatbeat.js
@@ -4,26 +4,29 @@ import { SharedArray } from 'k6/data';
 const userPool = new SharedArray('userPool', () => JSON.parse(open('./userPool.json')))
 const playbackToken = userPool[Math.floor(Math.random() * userPool.length)]
 
-const stages = []
-const totalStages = 10
-const duration = "30s"
-const rampingDuration = "30s"
-const vuIncrease = 50
+// const stages = []
+// const totalStages = 10
+// const duration = "30s"
+// const rampingDuration = "30s"
+// const vuIncrease = 50
 
-for(let i = 1; i <= totalStages; i ++){
-    stages.push({
-        duration: rampingDuration,
-        target: i * vuIncrease
-    })
+// for(let i = 1; i <= totalStages; i ++){
+//     stages.push({
+//         duration: rampingDuration,
+//         target: i * vuIncrease
+//     })
 
-    stages.push({
-        duration: duration,
-        target: i * vuIncrease
-    })
-}
+//     stages.push({
+//         duration: duration,
+//         target: i * vuIncrease
+//     })
+// }
 
 export const options = {
-    stages: stages
+    stages: [
+        {duration: "1m", target:500},
+        {duration: "2m", target:500}
+    ]
 };
 
 export default function () {

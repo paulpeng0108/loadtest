d560a36b79c2a34b396cbbe8fa2aa1a5545bb23f
Paul Peng <99743244+paulpeng0108@users.noreply.github.com>
2022-08-30 09:24:41 -0400

    commit

--- stream_index.js
+++ stream_index.js
@@ -2,16 +2,16 @@ import http from 'k6/http';
 
 export const options = {
     stages: [
-        { duration: '20s', target: 100 },
-        { duration: '20s', target: 100 },
-        { duration: '20s', target: 200 },
-        { duration: '20s', target: 200 },
-        { duration: '20s', target: 300 },
-        { duration: '20s', target: 300 },
-        { duration: '20s', target: 400 },
-        { duration: '20s', target: 400 },
-        { duration: '20s', target: 500 },
-        { duration: '20s', target: 500 }
+        { duration: '30s', target: 100 },
+        { duration: '2m', target: 100 },
+        { duration: '30s', target: 200 },
+        { duration: '2m', target: 200 },
+        { duration: '30s', target: 300 },
+        { duration: '2m', target: 300 },
+        { duration: '30s', target: 400 },
+        { duration: '2m', target: 400 },
+        { duration: '30s', target: 500 },
+        { duration: '2m', target: 500 }
     ]
 };
 

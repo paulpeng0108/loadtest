19e83bee9e8446a2b928756a6877775afb8e4245
Paul Peng <99743244+paulpeng0108@users.noreply.github.com>
2023-01-05 10:47:16 -0500

    commit

--- getStreamSource.js
+++ getStreamSource.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 
 export const options = {
     stages: [
-        { duration: '1s', target: 1000 }
+        { duration: '1s', target: 1000 },
         { duration: '0s', target: 0 }
     ]
 };
